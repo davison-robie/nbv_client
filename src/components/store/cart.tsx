87f6b418aea40ca4f7396229f08e5dddf2afbd5e
Davison Robie <AHDR@davisonsmacbook.attlocal.net>
2021-05-03 01:42:20 -0400

    view cart, view cart item complete

--- src/components/store/cart.tsx
+++ src/components/store/cart.tsx
@@ -1,14 +1,22 @@
+import { VerifyPublicKeyInput } from 'crypto';
 import { Component } from 'react';
-import { Table } from 'reactstrap';
+import { 
+    Modal, 
+    Table,
+    Button,
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter 
+} from 'reactstrap';
 
 interface iCartItem {
-    product_id: number;
-    cart_id: number;
-    name: string;
-    description: string;
-    price: number;
-    quantity: number;
-    image_url: string
+    product_id: number | undefined;
+    cart_id: number | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    price: number | undefined;
+    quantity: number | undefined;
+    image_url: string | undefined
 }
 
 export interface CartProps {
@@ -17,12 +25,28 @@ export interface CartProps {
  
 export interface CartState {
     cartItems: [iCartItem] | [];
+    oneCartItem: iCartItem;
+    modal: boolean;
+    detailModal: boolean;
 }
  
 class Cart extends Component<CartProps, CartState> {
     constructor(props: CartProps) {
         super(props);
-        this.state = { cartItems: [], };
+        this.state = { 
+            cartItems: [], 
+            oneCartItem: {
+                product_id: undefined, 
+                cart_id: undefined, 
+                name: undefined, 
+                description: undefined,
+                price: undefined,
+                quantity: undefined,
+                image_url: undefined
+            },
+            modal: false, 
+            detailModal: false,
+         };
     }
 
     fetchCart = () => {
@@ -47,30 +71,58 @@ class Cart extends Component<CartProps, CartState> {
         return this.state.cartItems.map((cartItem: iCartItem, index: number) => {
             
             return (
-                <tr>
+                <tr onClick={() => this.handleClick(cartItem)}>
                     <td>{cartItem.name}</td>
                     {/* <img src={cartItem.image_url} sizes="25px" alt="cart item image"></img> */}
                     <td>${cartItem.price}</td>
-                    {/* <Button onClick={(event) => this.handleClick(event, product)} >Add to Cart</Button> */}
                 </tr>
             )
         })
     }
 
+    toggle = () => this.setState({modal: !this.state.modal});
+    detailToggle = () => this.setState({detailModal: !this.state.detailModal});
+
+    handleClick = (cartItem: iCartItem) => {
+        this.detailToggle();
+        this.setState({oneCartItem: cartItem})
+    }
+
+    order = () => console.log("proceed to checkout");
+
     render() { 
         return (
+
             <div>
-                <Table responsive="md" hover>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.cartMapper()}
-                    </tbody>
-                </Table>
+                <Button color="danger" onClick={this.toggle}>View Cart</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className="cartStyle">
+                    <ModalHeader><h1>Cart</h1></ModalHeader>
+                    <ModalBody>
+                        <Table responsive="md" hover>
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.cartMapper()}
+                            </tbody>
+                        </Table>
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="danger" onClick={this.order}>Checkout</Button>
+                    </ModalFooter>
+                    <Modal isOpen={this.state.detailModal} toggle={this.detailToggle}>
+                    <ModalBody>
+                        <img src={this.state.oneCartItem.image_url} alt={this.state.oneCartItem.name} width="100%"/>
+                        <h2>{this.state.oneCartItem.name}</h2>
+                        <h2>{this.state.oneCartItem.description}</h2>
+                        <h2>{this.state.oneCartItem.price}</h2>
+                    </ModalBody>
+                    <ModalFooter></ModalFooter>
+                    </Modal>
+                </Modal>
             </div>
         );
     }
--- src/components/store/store_index.tsx
+++ src/components/store/store_index.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Modal } from 'reactstrap';
 import Inventory from "./inventory";
 import Cart from "./cart";
 
